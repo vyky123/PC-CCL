bb63accc44a74cec095a29a0efdbf0f98fa5822f
vyky123 <vyky0@seznam.cz>
2021-12-16 14:11:41 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/sem_prace_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sem_prace_2.dir/main.c.o"
+  "sem_prace_2"
+  "sem_prace_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sem_prace_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
